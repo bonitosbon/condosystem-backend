75d051d390bfbd75d237821fc2107e7e44472fe9
bonitosbon <vhoneuy@gmail.com>
2025-11-04 12:25:40 +0800

    Add database migration for BookingLink field

---
+++ Migrations/20251104042532_AddBookingLinkToCondo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CondoSystem_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingLinkToCondo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookingLink",
+                table: "Condos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookingLink",
+                table: "Condos");
+        }
+    }
+}
